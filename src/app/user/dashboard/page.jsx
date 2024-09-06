dcf7f8980c8b411121da8b76d700f075dc0b5ae9
Esxotic <wayanjerman63@gmail.com>
2024-09-06 15:00:58 +0800

    Menambahkan middleware

--- src/app/user/dashboard/page.jsx
+++ src/app/user/dashboard/page.jsx
@@ -1,15 +1,12 @@
-import { redirect } from "next/navigation";
 import { auth } from "@/services/auth";
 
 const Page = async () => {
   const session = await auth();
 
-  if (!session) return redirect("/");
-
   return (
     <div className="">
       <h3>Halaman Dashboard</h3>
-      <h1>Hallo {session.user.name}</h1>
+      <h1>Hallo {session?.user.name}</h1>
     </div>
   );
 };
