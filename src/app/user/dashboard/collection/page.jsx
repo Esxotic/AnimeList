8f4fd5757efc12f273f4558ccde91471c3ee3353
Esxotic <wayanjerman63@gmail.com>
2024-09-08 00:30:48 +0800

    Melanjutkan ui my collection

--- src/app/user/dashboard/collection/page.jsx
+++ src/app/user/dashboard/collection/page.jsx
@@ -1,8 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Page = () => {
   return (
-    <div className="">
-      <h1>Ini halaman collection</h1>
-    </div>
+    <section id="collection">
+      <h1 className="text-lg text-textPrimary font-bold text-center mt-2 capitalize">
+        My collection
+      </h1>
+      <div className="grid grid-cols-2 gap-4 justify-items-center px-4 mt-4 md:grid-cols-4">
+        <Link href={"/"} className="border-2 border-primary w-fit">
+          <Image
+            src="https://picsum.photos/300/300"
+            alt="..."
+            width={300}
+            height={350}
+          />
+          <h1 className="text-xs text-textPrimary font-bold bg-primary p-2 text-center md:text-sm">
+            Judul Anime
+          </h1>
+        </Link>
+        <Link href={"/"} className="border-2 border-primary w-fit">
+          <Image
+            src="https://picsum.photos/300/300"
+            alt="..."
+            width={300}
+            height={350}
+          />
+          <h1 className="text-xs text-textPrimary font-bold bg-primary p-2 text-center md:text-sm">
+            Judul Anime
+          </h1>
+        </Link>
+        <Link href={"/"} className="border-2 border-primary w-fit">
+          <Image
+            src="https://picsum.photos/300/300"
+            alt="..."
+            width={300}
+            height={350}
+          />
+          <h1 className="text-xs text-textPrimary font-bold bg-primary p-2 text-center md:text-sm">
+            Judul Anime
+          </h1>
+        </Link>
+        <Link href={"/"} className="border-2 border-primary w-fit">
+          <Image
+            src="https://picsum.photos/300/300"
+            alt="..."
+            width={300}
+            height={350}
+          />
+          <h1 className="text-xs text-textPrimary font-bold bg-primary p-2 text-center md:text-sm">
+            Judul Anime
+          </h1>
+        </Link>
+      </div>
+    </section>
   );
 };
 
